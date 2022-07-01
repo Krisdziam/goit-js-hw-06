9d5ea83b0fa7efc5ca6431a37211d97422ab6981
Kristian <kris.dziamka@gmail.com>
2022-07-01 20:27:09 +0300

    make 2 tasks

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,16 @@
 const listEl = document.querySelector(`#categories`);
-console.log(listEl)
+console.log(`Number of categories:`, listEl.children.length)
+
+
+
+const itemEl = document.querySelectorAll('.item');
+
+
+itemEl.forEach(event => {
+    console.log('Category:', event.firstElementChild.textContent);
+    console.log('Elements:', event.lastElementChild.children.length);
+  });
+
+
+
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,34 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector(`#ingredients`);
+
+// const markup = ingredients.map(option => {
+//   const item = document.createElement(`li`);
+// item.classList.add(`item`);
+// item.textContent = option;
+
+// return item;
+
+// });
+// listEl.append(...markup);
+
+const makeListEl = (options) => {
+  return options.map((option) => {
+    const item = document.createElement(`li`);
+    item.classList.add(`item`);
+    item.textContent = option;
+
+    return item;
+  });
+};
+
+const markup = makeListEl(ingredients);
+
+listEl.append(...markup);
